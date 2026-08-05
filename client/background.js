73e3ec11fd68ccb802b7c762a07cd33ee9ca5163
Ammar <166294346+doodle321@users.noreply.github.com>
2026-08-06 03:11:28 +0500

    Delete client/background.js

--- client/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessageExternal.addListener((msg, sender, sendResponse) => {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    if (tabs[0]) {
-      chrome.tabs.sendMessage(tabs[0].id, msg);
-    }
-  });
-});
